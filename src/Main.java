70f6f67a5b2fa76468e6ecc9b64baa8420941c65
YeshayahuBennun <ybennun@yahoo.com>
2023-02-12 18:54:45 +0200

    CircularQueueUsingArray_  TODO#1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        CircularQueue cq = new CircularQueue(3);
+
+        cq.enQueue(10);
+        cq.enQueue(20);
+        cq.enQueue(30);
+        cq.enQueue(40);
+    }
+}
